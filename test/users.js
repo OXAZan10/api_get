561fb518f6d7ba6b6cb2a219f7ebc2637112603b
Aksana Zanko <Aksana_Zanko@epam.com>
2021-02-18 23:06:23 +0300

    axios changes

--- test/users.js
+++ test/users.js
@@ -1,37 +1,30 @@
 const chai = require('chai');
 const expect = chai.expect;
-const sendRequest = require('../lib/sendRequest');
-const getUsers = require('../data/getUsers');
-const env = require('../endpoint/endpoint');
+const axios = require('axios').default;
 
 describe('Get users', () => {
+    let response;
 
-    getUsers.map((data) => {
-        let response;
-
-        before(async () => {
-            data.uri = env.uri + data.uri;
-            response = await sendRequest(data);
-            console.log(response);
-        });
-
-        it('status code is 200', () => {
-            console.log('status is ' + response.status);
-            expect(response.status).to.eql(200);
-        });
-
-        it('content-type header exists in the obtained response',()=>{
-            expect(response.header).to.have.property('content');
-            
-        });
-        it('the value of the content-type header is application/json; charset=utf-8',()=>{
-            expect()            
-        });
-        it('the content of the response body is the array of 10 users',()=>{
-            const usersFromRequest = response;
-            expect(usersFromRequest.length).to.eql(10)
-
-        });
+    before(async () => {
+        const receivedResponse = await axios.get('https://jsonplaceholder.typicode.com/users');
+        response = receivedResponse;
     });
 
+    it('status code is 200', () => {
+        const responseStatus=response.status;
+        expect(responseStatus).to.eql(200);
+    });
+
+    it('content-type header exists in the obtained response',()=>{
+        expect(response.headers).to.have.property('content-type');
+    });
+
+    it('the value of the content-type header is application/json; charset=utf-8',()=>{
+        const contentType = response.headers['content-type'];
+        expect(contentType).to.eql("application/json; charset=utf-8");            
+    });
+
+    it('the content of the response body is the array of 10 users',()=>{
+        expect(response.data.length).to.eql(10);
+    });
 });
